2b964d27e6437f5593599b9f9623e6e1952d257b
Amal <mehbinamal6@gmail.com>
2025-02-04 00:01:13 +0530

    SignUp Page Integrated With Backend

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,38 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import { handleError } from "../utils";
 
 function Signup(){
+
+    const [ SignupInfo,setSignupInfo] = useState({
+        name:'',
+        email:'',
+        password:'',
+    })
+
+    const handlechange = (e) => {
+        const { name,value } = e.target;
+        const copySignupInfo = { ...SignupInfo};
+        copySignupInfo[name] = value;
+        setSignupInfo(copySignupInfo);
+    }
+    
+    const handleSignup = async (e) => {
+        e.preventDefault();
+        const {name,email,password} = SignupInfo;
+        if(!name || !email || !password){
+            return handleError('All Fields Required');
+        }
+        try {
+            const url = 'http://localhost:8080/auth/signup';
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    },
+                body : JSON.stringify(SignupInfo)
+            });
+            const result = await response.json();
+            console.log(result);
+        } catch (error) {
+            handleError();
+        }
+    }
     return(
         <div className="container">
             <h1>Signup</h1>
-            <form>
+            <form onSubmit={handleSignup}>
                 <div>
                     <label htmlFor="name">Name</label>
                     <input
+                        onChange={handlechange}
                         type="text"
                         name="name"
                         autoFocus
                         placeholder="Enter Your Name"
+                        value={SignupInfo.name}
                     />
                 </div>
                 <div>
                     <label htmlFor="email">Email</label>
                     <input
+                        onChange={handlechange}
                         type="email"
                         name="email"
                         placeholder="Enter Your Email"
+                        value={SignupInfo.email}
                     />
                 </div>
                 <div>
                     <label htmlFor="password">Password</label>
                     <input
+                        onChange={handlechange}
                         type="password"
                         name="password"
                         placeholder="Enter Your password"
+                        value={SignupInfo.password}
                     />
                 </div>
-                <button>Signup</button>
+                <button type="submit">Signup</button>
                 <span>Already have an Account ? 
                     <Link to = "/login"> Login </Link>
                 </span>
@@ -42,4 +84,4 @@ function Signup(){
     )
 }
 
-export default Signup
+export default Signup;
