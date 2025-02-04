9df0e7a7ec09fa47fb65eddf9d724564a0faba25
Amal <mehbinamal6@gmail.com>
2025-02-04 15:17:04 +0530

    Basic Donor Home Page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Home(){
+    const [loggedInUser, setLoggedInUser] = useState('');
+    const navigate = useNavigate();
+    const handleLogout = (e) => {
+        localStorage.removeItem('token');
+        localStorage.removeItem('loggedInUser');
+        setTimeout(() =>{
+            navigate('/login');
+        },1000)
+    }
+    useEffect(()=>{
+        setLoggedInUser(localStorage.getItem('loggedInUser'))
+    },[])
     return(
-        <div> Home </div>
+        <div> 
+            <h1>{loggedInUser}</h1> 
+            <button onClick={handleLogout}>Logout</button>
+        </div>
     )
 }
 
