884a56b8db3a84d2978260f8f68174bc6bad5da3
Muhannadmuhammeali <mmamuhanned@gmail.com>
2025-03-25 23:14:51 +0530

    update menubar hovering issue ,login page white  text and about js file

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -18,6 +18,7 @@ function Navbar() {
           <Link to='#' className='menu-bars'>
             <FaIcons.FaBars onClick={showSidebar} />
           </Link>
+          <h1 className='website-name'>LifeLink</h1> {/* Added Website Name */}
         </div>
         <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
           <ul className='nav-menu-items' onClick={showSidebar}>
