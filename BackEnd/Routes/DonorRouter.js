2e3ac05dc8e3a39d02f958c5d13586650cacf12d
Mehbinamal <mehbinamal6@gmail.com>
2025-03-26 02:35:31 +0530

    Donation History Api Completed

--- BackEnd/Routes/DonorRouter.js
+++ BackEnd/Routes/DonorRouter.js
@@ -3,12 +3,13 @@ const express = require("express");
 const router = express.Router();
 const { getAllBloodRequests, getBloodRequestByID } = require("../Controllers/RequestRetrievalController");
 const { validateRequestRetrieval } = require("../Middlewares/RequestRetrievalValidator");
-const { acceptRequest } = require('../Controllers/donationController');
+const { acceptRequest,getDonationHistory } = require('../Controllers/donationController');
 const validateRequest = require('../Middlewares/validateRequest');
 
 router.get("/bloodRequests", validateRequestRetrieval, getAllBloodRequests);
 router.get("/detailedRequest/:id", validateRequestRetrieval, getBloodRequestByID);
 router.post('/acceptRequest/:requestId/:donorEmail', validateRequest, acceptRequest)
+router.get('/donationHistory/:email', getDonationHistory);
 
 
 module.exports = router;
